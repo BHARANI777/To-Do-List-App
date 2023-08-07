43ae9abe34d8162d340ad0b49a5f78fc07e45f8d
Your Name <youremail@domain.com>
2023-08-07 20:45:45 +0530

    ; changed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const clear = document.querySelector(".clear");
 const dateElement = document.getElementById("date");
 const list = document.getElementById("list");
-const input = document.getElementById("input");
+const input = document.getElementById("input")
 console.log(dateElement);
 
 //classes names
